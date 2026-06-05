c60e51232cdcec22f1f35b2616c55f474068f11c
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-05 17:54:28 +0530

    Time: 24 ms (5.76%) | Memory: 44.4 MB (5.72%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValid(String s) {
+        List<Character> stack = new LinkedList<>();
+
+        for (char c : s.toCharArray()) {
+            // System.out.println("for C: " + c);
+            if (stack.isEmpty()) {
+                stack.add(c);
+                // System.out.println("stack: "  + stack);
+                continue;
+            }
+            
+            // if it is opening bracket then put in stack
+            if (c == '(' || c == '{' || c == '[') {
+                // System.out.println("Opening B: "  + c);
+                stack.add(c);
+                // System.out.println("stack: "  + stack);
+                continue;
+            }
+            
+            // if it is closing bracket 
+            if (c == ')' || c == '}' || c == ']') {
+                // System.out.println("Closing B: "  + c);
+                // peek element of stack which is last open bracket
+                char lob = stack.get(stack.size() - 1);
+                // System.out.println("lob: "  + lob);
+                char op = getOp(c);
+                System.out.println("op: "  + op);
+                if (lob == op) {
+                    // System.out.println("same hai");
+                    stack.remove(stack.size() - 1);
+                } else {
+                    return false;
+                }
+                
+                // System.out.println("stack: "  + stack);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    char getOp(char c) {
+        if (c == ')') return '(';
+        if (c == '}') return '{';
+        if (c == ']') return '[';
+        return 'a';
+    }
+}
