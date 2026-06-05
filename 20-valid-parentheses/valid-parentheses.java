49f84a2000fa360d4d6dbf3043ff176933c55719
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-05 18:57:14 +0530

    Time: 5 ms (14.75%) | Memory: 43.5 MB (15.21%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,37 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
-            // System.out.println("for C: " + c);
-            if (stack.isEmpty()) {
+            if (stack.isEmpty() || c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-                // System.out.println("stack: "  + stack);
                 continue;
             }
             
-            // if it is opening bracket then put in stack
-            if (c == '(' || c == '{' || c == '[') {
-                // System.out.println("Opening B: "  + c);
-                stack.push(c);
-                // System.out.println("stack: "  + stack);
-                continue;
-            }
-            
-            // if it is closing bracket 
             if (c == ')' || c == '}' || c == ']') {
-                // System.out.println("Closing B: "  + c);
-                // peek element of stack which is last open bracket
                 char lob = stack.peek();
-                // System.out.println("lob: "  + lob);
                 char op = getOp(c);
-                // System.out.println("op: "  + op);
-                if (lob == op) {
-                    // System.out.println("same hai");
-                    stack.pop();
-                } else {
-                    return false;
-                }
-                
-                // System.out.println("stack: "  + stack);
+                if (lob != op) return false;
+                stack.pop();
             }
         }
         return stack.isEmpty();
