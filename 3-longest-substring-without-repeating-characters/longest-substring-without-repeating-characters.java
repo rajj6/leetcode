de10f79cda7ad156b628faae01b4dc106beaa6d5
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-05 19:54:44 +0530

    Time: 5 ms (86.52%) | Memory: 45.7 MB (68.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,30 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
-        String subS;
-        for(int i = 0; i < s.length() ; i++) {
-            for (int j = i + 1; j <=  s.length(); j++) {
-                // System.out.println("For Sub i: " + i + " j: " + j);
-                // Get Each Sub String
-                // System.out.println(s.substring(i, j));
-                subS = s.substring(i, j);
-                if (j - i > 100) { i++ ; continue;}
-                // For each sub string check if contains duplicate
-                if (!containsDuplicate(s.substring(i, j))) {
-                    max = Math.max(max, subS.length());
+        Set<Character> seen = new HashSet<>();
+        int i = 0;
+        char[] chars = s.toCharArray();
+        for (int j = 0; j < chars.length; j++) {
+            char c = chars[j];
+            if (j == 0) {
+                seen.add(c);
+            } else {
+                if (seen.contains(c)) {
+                    // part of i to j seq
+                    
+                    while (chars[i] != c) {
+                        seen.remove(chars[i]);
+                        i++;
+                    }
+                    if (chars[i] == c) {
+                        i++;
+                        continue;
+                    }
+                    seen.add(c);
+                } else {
+                    seen.add(c);
                 }
             }
+            max = Math.max(max, seen.size());
         }
         return max;
-    }
-
-    private boolean containsDuplicate(String s) {
-        Set<Character> seen = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            if(!seen.add(c)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
