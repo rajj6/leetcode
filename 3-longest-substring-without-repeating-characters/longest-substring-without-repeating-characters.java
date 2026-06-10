e50f265d8f5873e3c6296a7acca8832c09e44eaf
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-10 22:41:34 +0530

    Time: 6 ms (69.31%) | Memory: 46.3 MB (53.82%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,25 +1,42 @@
 class Solution {
+    // Ugly code but works
+    // public int lengthOfLongestSubstring(String s) {
+    //     int max = 0;
+    //     Set<Character> seen = new HashSet<>();
+    //     int i = 0;
+    //     char[] chars = s.toCharArray();
+    //     for (int j = 0; j < chars.length; j++) {
+    //         char c = chars[j];
+    //         if (j == 0) {
+    //             seen.add(c);
+    //         } else {
+    //             if (seen.contains(c)) {
+    //                 while (chars[i] != c) {
+    //                     seen.remove(chars[i]);
+    //                     i++;
+    //                 }
+    //                 i++;
+    //             } else {
+    //                 seen.add(c);
+    //             }
+    //         }
+    //         max = Math.max(max, seen.size());
+    //     }
+    //     return max;
+    // }
+
     public int lengthOfLongestSubstring(String s) {
-        int max = 0;
         Set<Character> seen = new HashSet<>();
-        int i = 0;
+        int max = 0;
         char[] chars = s.toCharArray();
-        for (int j = 0; j < chars.length; j++) {
-            char c = chars[j];
-            if (j == 0) {
-                seen.add(c);
-            } else {
-                if (seen.contains(c)) {
-                    // part of i to j seq
-                    while (chars[i] != c) {
-                        seen.remove(chars[i]);
-                        i++;
-                    }
-                    i++;
-                } else {
-                    seen.add(c);
-                }
+        int j = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            while (seen.contains(c)) {
+                seen.remove(chars[j]);
+                j++;
             }
+            seen.add(c);
             max = Math.max(max, seen.size());
         }
         return max;
