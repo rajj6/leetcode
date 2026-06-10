27ef77475b3ff4079629aa29f7bdbe7e5a1d4e36
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-10 22:30:03 +0530

    Time: 2 ms (70.13%) | Memory: 46.9 MB (93.22%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,40 @@
 class Solution {
+
+    // Using prefix and sufix approch
+    // public int maxProduct(int[] nums) {
+    //     int pref = 1, suf = 1, n = nums.length;
+    //     int max = nums[0];
+    //     for (int i = 0; i < n; i++) {
+    //         if (pref == 0)  pref = 1;
+    //         if (suf == 0)   suf = 1;
+
+    //         pref    *= nums[i];
+    //         suf     *= nums[n-i-1];
+    //         max = Math.max(max, Math.max(pref, suf));
+
+    //     }
+    //     return max;
+    // }
+
+
+    // Using kadens approch
     public int maxProduct(int[] nums) {
-        int pref = 1, suf = 1, n = nums.length;
-        int max = nums[0];
-        for (int i = 0; i < n; i++) {
-            if (pref == 0)  pref = 1;
-            if (suf == 0)   suf = 1;
-
-            pref    *= nums[i];
-            suf     *= nums[n-i-1];
-            max = Math.max(max, Math.max(pref, suf));
-
+        int globalMax = nums[0];
+        int curMax = nums[0];
+        int curMin = nums[0];
+        int num,temp;
+        for (int i = 1; i < nums.length; i++) {
+            num = nums[i];
+            if (num < 0) {
+                temp = curMax;
+                curMax = curMin;
+                curMin = temp;
+            }
+            curMax = Math.max(curMax*num,num);
+            curMin = Math.min(curMin*num, num);
+            globalMax = Math.max(globalMax, curMax);
         }
-        return max;
+        return globalMax;
     }
+
 }
