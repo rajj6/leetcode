7849c76d88809bb7d9f03d77a089b3f721652bbf
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-06-23 20:44:03 +0530

    Time: 8 ms (16.03%) | Memory: 48 MB (7.94%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Brute force
-        // For each number from 0 to 9 search the array if not found return
+        // Brute force:: For each number from 0 to 9 search the array if not found return
+        /*
         for (int i = 0; i <= nums.length; i++) {
             boolean found = false;
             for (int j = 0; j < nums.length; j++) {
-                if(nums[j] == i) {
+                if (nums[j] == i) {
                     found = true;
                     break;
                 }
@@ -13,5 +13,15 @@ class Solution {
             if (!found) return i;
         }
         return 0;
+        */
+        // try sorting in 1 loop :: for index i if i != nums[i]
+        Arrays.sort(nums);
+        int i = 0;
+        if(nums[i] != 0) return 0;
+        while (i < nums.length-1) {
+            if(nums[i]+1 != nums[i+1]) return nums[i] + 1;
+            i++;
+        }
+        return nums[i]  + 1;
     }
 }
