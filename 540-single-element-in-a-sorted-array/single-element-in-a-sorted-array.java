993b13a61eafdf0bc3dac32bf5795eccb34d0d7c
Krishnkant Jaiswal <39212433+rajj6@users.noreply.github.com>
2026-07-02 20:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (57.48%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -2,29 +2,39 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
 
         // Brute force:
-        if (nums.length == 1) return nums[0];
-         for(int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] != nums[i+1]) return nums[i];
-            } else if (i == nums.length - 1) {
-                if (nums[i] != nums[i-1]) return nums[i];
-            } else {
-                if (nums[i] != nums[i+1] && nums[i] != nums[i-1]) return nums[i];
-            }
-        }
-        // int left = 0, right = nums.length - 1;
-        // while (left <= right) {
-        //     int mid = (left + right)/2;
-        //     if (left == right) {
-        //         return nums[mid];
-        //     } else if (nums[mid] == nums[mid-1]) {
-        //         right =  mid - 2;
-        //     } else if (nums[mid] == nums[mid + 1]) {
-        //         left = mid + 2;
+        // if (nums.length == 1) return nums[0];
+        //  for(int i = 0; i < nums.length; i++) {
+        //     if (i == 0) {
+        //         if (nums[i] != nums[i+1]) return nums[i];
+        //     } else if (i == nums.length - 1) {
+        //         if (nums[i] != nums[i-1]) return nums[i];
         //     } else {
-        //         return nums[mid];
+        //         if (nums[i] != nums[i+1] && nums[i] != nums[i-1]) return nums[i];
         //     }
         // }
+        if (nums.length == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        int n = nums.length;
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if ( nums[mid + 1] != nums[mid] && nums[mid] != nums[mid - 1]) return nums[mid];
+            if (mid % 2 == 1) {
+                if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
         return -1;
     }
 }
